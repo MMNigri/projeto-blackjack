9e84e2add9a51908e309e9661b32fe9c09fe0142
MMN <marcos_marvila@hotmail.com>
2023-06-02 19:40:43 -0300

    Projeto V1 para correção

--- versao_1.js
+++ versao_1.js
@@ -1,3 +1,34 @@
+confirm("Bem vindo(a) ao Jogo de Blak Jack")
+
+let partida = confirm("Gostaria de jogar uma rodada ?")
+
+if(partida){
+
+let card1User = comprarCarta()
+let card2User = comprarCarta()
+let card1Pc = comprarCarta()
+let card2Pc = comprarCarta()
+
+let userScore = card1User.valor + card2User.valor
+let pcScore = card1Pc.valor + card2Pc.valor
+
+console.log(`Suas cartas são: ${card1User.texto} e ${card2User.texto} Pontuação: ${userScore}`)
+console.log(`Cartas do oponente: ${card1Pc.texto} e ${card2Pc.texto} Pontuação ${pcScore} `)
+
+if (userScore > pcScore){
+   console.log("Você Ganhou !!!")
+}else if(userScore < pcScore){
+   console.log("Uma pena, você perdeu")
+}else if(userScore === pcScore){
+   console.log("Empate !")
+}
+
+}else{
+   console.log("Fim de Jogo")
+}
+
+
+
 /**
  * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
  * 
